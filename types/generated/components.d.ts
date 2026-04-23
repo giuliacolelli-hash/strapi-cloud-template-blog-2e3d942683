60aa167c4912ee6c7734070c20afd860c950b739
giuliacolelli-hash <giulia.colelli@fifthbeat.com>
2026-04-23 12:07:41 +0200

    prova1

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,53 +1,66 @@
-import type { Struct, Schema } from '@strapi/strapi';
+import type { Schema, Struct } from '@strapi/strapi';
 
-export interface SharedSlider extends Struct.ComponentSchema {
-  collectionName: 'components_shared_sliders';
+export interface ProvaCategoryOverview extends Struct.ComponentSchema {
+  collectionName: 'components_prova_category_overviews';
   info: {
-    displayName: 'Slider';
-    icon: 'address-book';
-    description: '';
+    displayName: 'Category overview';
+    icon: 'bulletList';
   };
   attributes: {
-    files: Schema.Attribute.Media<'images', true>;
+    catBlurb: Schema.Attribute.Text & Schema.Attribute.Required;
+    catTitle: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
-export interface SharedSeo extends Struct.ComponentSchema {
-  collectionName: 'components_shared_seos';
+export interface ProvaHeroImg extends Struct.ComponentSchema {
+  collectionName: 'components_prova_hero_imgs';
   info: {
-    name: 'Seo';
-    icon: 'allergies';
-    displayName: 'Seo';
-    description: '';
+    displayName: 'Hero Img';
+    icon: 'picture';
   };
   attributes: {
-    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
-    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
-    shareImage: Schema.Attribute.Media<'images'>;
+    heroimg: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
   };
 }
 
-export interface SharedRichText extends Struct.ComponentSchema {
-  collectionName: 'components_shared_rich_texts';
+export interface ProvaMediaGallery extends Struct.ComponentSchema {
+  collectionName: 'components_prova_media_galleries';
   info: {
-    displayName: 'Rich text';
-    icon: 'align-justify';
-    description: '';
+    displayName: 'Media gallery';
+    icon: 'dashboard';
   };
   attributes: {
-    body: Schema.Attribute.RichText;
+    files: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
   };
 }
 
-export interface SharedQuote extends Struct.ComponentSchema {
-  collectionName: 'components_shared_quotes';
+export interface ProvaProjectOverview extends Struct.ComponentSchema {
+  collectionName: 'components_prova_project_overviews';
   info: {
-    displayName: 'Quote';
-    icon: 'indent';
+    displayName: 'Project overview';
+    icon: 'play';
   };
   attributes: {
-    title: Schema.Attribute.String;
-    body: Schema.Attribute.Text;
+    projBlurb: Schema.Attribute.Text & Schema.Attribute.Required;
+    projImg: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'> &
+      Schema.Attribute.Required;
+    projTitle: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface ProvaTextBox extends Struct.ComponentSchema {
+  collectionName: 'components_prova_text_boxes';
+  info: {
+    displayName: 'Text box';
+    icon: 'write';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText & Schema.Attribute.Required;
+    media: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    shortTitle: Schema.Attribute.String & Schema.Attribute.Required;
   };
 }
 
@@ -62,14 +75,70 @@ export interface SharedMedia extends Struct.ComponentSchema {
   };
 }
 
+export interface SharedQuote extends Struct.ComponentSchema {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'indent';
+  };
+  attributes: {
+    body: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    shareImage: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    files: Schema.Attribute.Media<'images', true>;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'shared.slider': SharedSlider;
-      'shared.seo': SharedSeo;
-      'shared.rich-text': SharedRichText;
-      'shared.quote': SharedQuote;
+      'prova.category-overview': ProvaCategoryOverview;
+      'prova.hero-img': ProvaHeroImg;
+      'prova.media-gallery': ProvaMediaGallery;
+      'prova.project-overview': ProvaProjectOverview;
+      'prova.text-box': ProvaTextBox;
       'shared.media': SharedMedia;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
     }
   }
 }
